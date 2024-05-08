2d2e8eb1c4eed344f57efbc9ce3ff0639d5d6e2c
erastogodi <erastorodas@alunos.utfpr.edu.br>
2024-05-08 19:35:33 -0300

    inserir model proposta

---
+++ Model/proposta.php
@@ -0,0 +1,28 @@
+<?php
+// ../model/proposta.php
+
+class Proposta {
+    private $mysqli;
+
+    public function __construct($mysqli) {
+        $this->mysqli = $mysqli;
+    }
+
+    public function enviarProposta($nome_evento, $desc_evento, $data_evento, $local_evento, $info_contato) {
+        if ($this->mysqli->connect_error) {
+            return "Erro na conexão com o banco de dados: " . $this->mysqli->connect_error;
+        }
+
+        $stmt = $this->mysqli->prepare("INSERT INTO proposta_evento (nome_evento, desc_evento, data_evento, local_evento, info_contato) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssss", $nome_evento, $desc_evento, $data_evento, $local_evento, $info_contato);
+
+        if ($stmt->execute()) {
+            return "Proposta de evento enviada com sucesso!";
+        } else {
+            return "Erro ao enviar proposta de evento: " . $stmt->error;
+        }
+
+        $stmt->close();
+    }
+}
+?>
